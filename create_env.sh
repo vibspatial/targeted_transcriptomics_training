925ba965c16f015f9bcd15e40bd6909c2c664008
ArneDefauw <arnedefauw@gmail.com>
2026-03-08 20:51:49 +0100

    test run notebooks on macOS

--- create_env.sh
+++ create_env.sh
@@ -1,8 +1,9 @@
-uv venv .venv_harpy --python 3.13
+# cellpose==3.1.1.3 pulls in numpy==2.0.2 on macOS, which does not build on Python 3.13.
+uv venv .venv_harpy --python 3.12
 source .venv_harpy/bin/activate
 
 uv pip install 'harpy-analysis[extra] @ git+https://github.com/saeyslab/harpy.git@main'
-#uv pip install 'harpy-analysis[napari] @ git+https://github.com/saeyslab/harpy.git@main' # do not install napari on vib compute
+uv pip install 'harpy-analysis[napari] @ git+https://github.com/saeyslab/harpy.git@main' # do not install napari on vib compute
 uv pip install squidpy
 uv pip install cellpose==3.1.1.3
 uv pip install jupyter
