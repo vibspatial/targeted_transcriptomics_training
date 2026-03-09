53a1a51ea27ca3a8de4c4652338dd1b130cc9c2a
ArneDefauw <arnedefauw@gmail.com>
2026-03-09 08:02:40 +0100

    create env

--- create_env.sh
+++ create_env.sh
@@ -2,8 +2,7 @@
 uv venv .venv_harpy --python 3.12
 source .venv_harpy/bin/activate
 
-uv pip install 'harpy-analysis[extra] @ git+https://github.com/saeyslab/harpy.git@main'
-uv pip install 'harpy-analysis[napari] @ git+https://github.com/saeyslab/harpy.git@main' # do not install napari on vib compute
+uv pip install 'harpy-analysis[extra,napari] @ git+https://github.com/saeyslab/harpy.git@main'
 uv pip install squidpy
 uv pip install cellpose==3.1.1.3
 uv pip install jupyter
