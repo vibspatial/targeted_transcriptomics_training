00b39cf2c28a94d5370b595a2bf2c2cd28b7e436
ArneDefauw <arnedefauw@gmail.com>
2026-05-04 11:24:16 +0200

    create env

--- create_env.sh
+++ create_env.sh
@@ -1,10 +1,13 @@
 # cellpose==3.1.1.3 pulls in numpy==2.0.2 on macOS, which does not build on Python 3.13.
-uv venv .venv_harpy --python 3.12
-source .venv_harpy/bin/activate
+uv venv .venv --python 3.12
+source .venv/bin/activate
 
 uv pip install 'harpy-analysis[extra,napari] @ git+https://github.com/saeyslab/harpy.git@main'
 uv pip install "harpy_vitessce @ git+https://github.com/vibspatial/harpy_vitessce.git@main"
 uv pip install squidpy
 uv pip install cellpose==3.1.1.3
 uv pip install jupyter
+uv pip install mofax
+uv pip install mofapy2
 uv pip install bokeh
+
